c237f1c04ea44d5eebfca745feebb2fdd20b1a46
tigerphc <47968763+tigerphc@users.noreply.github.com>
2020-02-17 15:36:44 +0800

    commit new files.

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="nav-bar">
+    <el-row :gutter="0">
+      <el-col :span="10" align="center"><div><slot name="left"></slot></div></el-col>
+      <el-col :span="12" align="center"><div><slot name="center"></slot></div></el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+	export default {
+		name: "NavBar"
+	}
+</script>
+
+<style scoped>
+  #nav-bar {
+    position: relative;
+    z-index: 10;
+
+    /* display: flex; */
+
+    height: 44px;
+    line-height: 44px;
+    text-align: center;
+    background-color: #74B243;
+  }
+
+
+</style>
---
+++ src/views/home/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <nav-bar class="nav-bar">
+      <div slot='left'><img src="~assets/img/logo.png" alt="" style="height:44px;"></div>
+      <div slot="center">
+        <el-input size="small" v-model="searchItem" placeholder="材料名，料理名" prefix-icon="el-icon-search"></el-input>
+      </div>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+import NavBar from 'common/navbar/NavBar'
+export default {
+  name: "Home",
+  components: {
+    NavBar,
+
+  },
+  data() {
+     return {
+      searchItem: '',
+     }
+
+    }
+
+}
+</script>
+
+<style scoped>
+
+</style>
